bfaccf95fa78d9720ee66af7f36aaba65d587e7b
施振宇 <2889035022@qq.com>
2022-02-22 13:23:58 +0800

    增加state预测的训练完成

--- main.py
+++ main.py
@@ -274,7 +274,7 @@ if __name__ == '__main__':
     # for training
     parser = argparse.ArgumentParser('DETR training and evaluation script', parents=[get_args_parser()])
     args = parser.parse_args()
-    args.output_dir = './results_pretrain_complete'
+    args.output_dir = './results_pretrain_state'
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     args.batch_size = 4
