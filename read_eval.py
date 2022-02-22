e98afa291925a2254b5202149d1f70bce0dc7343
施振宇 <2889035022@qq.com>
2022-02-22 20:36:54 +0800

    对state预测的结果进行数据分析

--- read_eval.py
+++ read_eval.py
@@ -47,7 +47,6 @@ def read_checkpoint():
     torch.save({'model': M.state_dict()}, path)
 
 
-
 def build_new_model():
     import os
     from main import get_args_parser
@@ -78,8 +77,70 @@ def build_new_model():
     return model
 
 
+def read_log():
+    import json
+    log_path = '/home/szy/detr/results_pretrain_state/log.txt'
+    log_file = open(log_path, 'r')
+    data_dict_1 = {'train_class_error': [], 'train_state_error_unscaled': [], 'epoch': []}
+    data_dict_2 = {'train_loss_ce': [], 'train_loss_se_unscaled': [], 'epoch': []}
+    data_dict_3 = {'train_class_error': [], 'test_class_error': [], 'epoch': []}
+    data_dict_4 = {'train_state_error_unscaled': [], 'test_state_error_unscaled': [], 'epoch': []}
+    for line in log_file:
+        # json.loads()可以字典格式的str转换成dict
+        log_dic = json.loads(line)
+        for key in log_dic.keys():
+            if key == 'epoch':
+                data_dict_1[key].append(log_dic[key])
+                data_dict_2[key].append(log_dic[key])
+                data_dict_3[key].append(log_dic[key])
+                data_dict_4[key].append(log_dic[key])
+            if key == 'train_class_error':
+                data_dict_1[key].append(log_dic[key])
+                data_dict_3[key].append(log_dic[key])
+            if key == 'train_state_error_unscaled':
+                data_dict_1[key].append(log_dic[key])
+                data_dict_4[key].append(log_dic[key])
+            if key == 'train_loss_ce':
+                data_dict_2[key].append(log_dic[key])
+            if key == 'train_loss_se_unscaled':
+                data_dict_2[key].append(log_dic[key])
+            if key == 'test_state_error_unscaled':
+                data_dict_4[key].append(log_dic[key])
+            if key == 'test_class_error':
+                data_dict_3[key].append(log_dic[key])
+
+    plot(data_dict_1)
+
+
+def plot(data_dict):
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(10, 5))  # 设置画布的尺寸
+    plt.title('Class and State error', fontsize=20)  # 标题，并设定字号大小
+    plt.xlabel(u'epoch', fontsize=14)  # 设置x轴，并设定字号大小
+    plt.ylabel(u'error', fontsize=14)  # 设置y轴，并设定字号大小
+
+    y = {}
+    for key, value in data_dict.items():
+        if 'epoch' in key:
+            x = data_dict[key]
+        else:
+            y[key] = data_dict[key]
+
+    color = ["deeppink", "darkblue"]
+    i = 0
+    for key, value in y.items():
+        # color：颜色，linewidth：线宽，linestyle：线条类型，label：图例，marker：数据点的类型
+        plt.plot(x, value, color=color[i], linewidth=2, linestyle=':', label=key, marker='o')
+        i = i+1
+
+    plt.legend(loc=2)  # 图例展示位置，数字代表第几象限
+    plt.savefig('./1.png')  # 保存图片
+    plt.show()  # 显示图像
+
 
 if __name__ == '__main__':
     #main()
-    read_checkpoint()
+    #read_checkpoint()
     #build_new_model()
+    read_log()
